e02550a3e61a9cce00f3bf47f690b868dc3ab7f7
peacefulotter <zankro67@gmail.com>
2024-07-21 18:50:57 +0200

    Cleaning p3 codfe

--- src/main/scala/p3_longest_substring.scala
+++ src/main/scala/p3_longest_substring.scala
@@ -1,68 +1,28 @@
 package com.peacefulotter.leetcode
 
 import scala.annotation.tailrec
-import scala.collection.mutable
 
 object p3_longest_substring {
   def lengthOfLongestSubstring(s: String): Int = {
+    @tailrec
     def rec(
         s: String,
         cur: Vector[Char] = Vector(),
         best: Int = 0
     ): Int = {
-      println(s, cur, best)
       // small optimization to break the recursion sooner
       if (s.isEmpty || s.length + cur.size < best) Math.max(best, cur.size)
       else
         (s.head, s.tail, cur.indexOf(s.head)) match {
-          case (c: Char, t: String, i: Int) if i >= 0 =>
-            rec(
-              t,
-              (cur.zipWithIndex collect { case (x, j) if j > i => x }) :+ c,
-              Math.max(best, cur.size)
-            )
+          case (c: Char, t: String, i: Int) if i >= 0 => {
+            val newCur =
+              (cur.zipWithIndex collect { case (x, j) if j > i => x }) :+ c
+            rec(t, newCur, Math.max(best, cur.size))
+          }
           case _ => rec(s.tail, cur :+ s.head, best)
         }
     }
-    rec(s)
 
-//    @tailrec
-//    def append(t: Vector[Char], c: Char): Vector[Char] =
-//      if (t.contains(c)) append(t.tail, c) else t :+ c
-//
-//    def rec(acc: Vector[Char], i: Int): Int = {
-//      if (i == s.length)
-//        acc.length
-//      else {
-//        val c = s(i)
-//        Math.max(
-//          if (acc.contains(c)) acc.length else rec(acc :+ c, i + 1),
-//          rec(append(acc, c), i + 1)
-//        )
-//      }
-//    }
-//
-//    rec(Vector[Char](), 0)
+    rec(s)
   }
-//    def lengthOfLongestSubstring(s: String): Int = {
-//        def append(t: ListBuffer[Char], c: Char): ListBuffer[Char] = {
-//            var t_ = t
-//            while (t_.contains(c))
-//                t_ = t_.tail
-//            t_ :+ c
-//        }
-//
-//        def rec(acc: ListBuffer[Char], i: Int): Int = {
-//            // println(s + " " + acc + " " + i + " " + s(i) + " " + acc.contains(s(i)))
-//            if (i == s.length)
-//                acc.size
-//            else
-//                Math.max(
-//                    if (acc.contains(s(i))) acc.size else rec(acc :+ s(i), i + 1),
-//                    rec(append(acc.clone(), s(i)), i + 1)
-//                )
-//        }
-//
-//        rec(ListBuffer[Char](), 0)
-//    }
 }
