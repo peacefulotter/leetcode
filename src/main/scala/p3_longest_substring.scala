4ee48592302179b2ec005a16725a44c883e571a1
peacefulotter <zankro67@gmail.com>
2024-07-21 18:48:25 +0200

    Solved p3 (finally)

--- src/main/scala/p3_longest_substring.scala
+++ src/main/scala/p3_longest_substring.scala
@@ -1,28 +1,49 @@
 package com.peacefulotter.leetcode
 
 import scala.annotation.tailrec
+import scala.collection.mutable
 
 object p3_longest_substring {
-    def lengthOfLongestSubstring(s: String): Int = {
-        @tailrec
-        def append(t: Vector[Char], c: Char): Vector[Char] =
-            if (t.contains(c)) append(t.tail, c) else t :+ c
-
-        def rec(acc: Vector[Char], i: Int): Int = {
-            // println(s + " " + acc + " " + i + " " + s(i) + " " + acc.contains(s(i)))
-            if (i == s.length)
-                acc.length
-            else {
-                val c = s(i)
-                Math.max(
-                    if (acc.contains(c)) acc.length else rec(acc :+ c, i + 1),
-                    rec(append(acc, c), i + 1)
-                )
-            }
+  def lengthOfLongestSubstring(s: String): Int = {
+    def rec(
+        s: String,
+        cur: Vector[Char] = Vector(),
+        best: Int = 0
+    ): Int = {
+      println(s, cur, best)
+      // small optimization to break the recursion sooner
+      if (s.isEmpty || s.length + cur.size < best) Math.max(best, cur.size)
+      else
+        (s.head, s.tail, cur.indexOf(s.head)) match {
+          case (c: Char, t: String, i: Int) if i >= 0 =>
+            rec(
+              t,
+              (cur.zipWithIndex collect { case (x, j) if j > i => x }) :+ c,
+              Math.max(best, cur.size)
+            )
+          case _ => rec(s.tail, cur :+ s.head, best)
         }
-
-        rec(Vector[Char](), 0)
     }
+    rec(s)
+
+//    @tailrec
+//    def append(t: Vector[Char], c: Char): Vector[Char] =
+//      if (t.contains(c)) append(t.tail, c) else t :+ c
+//
+//    def rec(acc: Vector[Char], i: Int): Int = {
+//      if (i == s.length)
+//        acc.length
+//      else {
+//        val c = s(i)
+//        Math.max(
+//          if (acc.contains(c)) acc.length else rec(acc :+ c, i + 1),
+//          rec(append(acc, c), i + 1)
+//        )
+//      }
+//    }
+//
+//    rec(Vector[Char](), 0)
+  }
 //    def lengthOfLongestSubstring(s: String): Int = {
 //        def append(t: ListBuffer[Char], c: Char): ListBuffer[Char] = {
 //            var t_ = t
--- src/test/scala/p3_longest_substring_test.scala
+++ src/test/scala/p3_longest_substring_test.scala
@@ -3,40 +3,44 @@ package com.peacefulotter.leetcode
 import org.scalatest.flatspec.AnyFlatSpec
 
 class p3_longest_substring_test extends AnyFlatSpec {
-    private def test(s: String, i: Int) = {
-        assert(p3_longest_substring.lengthOfLongestSubstring(s) === i)
-    }
+  private def test(s: String, i: Int) = {
+    assert(p3_longest_substring.lengthOfLongestSubstring(s) === i)
+  }
 
-    it should "abcabcbb = 3" in {
-        test("abcabcbb", 3)
-    }
+  it should "' ' = 1" in {
+    test(" ", 1)
+  }
 
-    it should "bbbbb = 1" in {
-        test("bbbbb", 1)
-    }
+  it should "abcabcbb = 3" in {
+    test("abcabcbb", 3)
+  }
 
-    it should "pwwkew = 3" in {
-        test("pwwkew", 3)
-    }
+  it should "bbbbb = 1" in {
+    test("bbbbb", 1)
+  }
 
-    it should "qrsvbspk = 5" in {
-        test("qrsvbspk", 5)
-    }
+  it should "pwwkew = 3" in {
+    test("pwwkew", 3)
+  }
 
-    it should "xsmxvvwxhdylaeva = 9" in {
-        test("xsmxvvwxhdylaeva", 9)
-    }
+  it should "qrsvbspk = 5" in {
+    test("qrsvbspk", 5)
+  }
 
-    it should "taxliekzrzmqkxhffoilxebcb = 9" in {
-        test("taxliekzrzmqkxhffoilxebcb", 9)
-    }
+  it should "xsmxvvwxhdylaeva = 9" in {
+    test("xsmxvvwxhdylaeva", 9)
+  }
 
-    it should "awtmojhtslcnfdpffakysphq = 12" in {
-        test("awtmojhtslcnfdpffakysphq", 12)
-    }
+  it should "taxliekzrzmqkxhffoilxebcb = 9" in {
+    test("taxliekzrzmqkxhffoilxebcb", 9)
+  }
 
-    it should "astabfulijqqxvzpxbbpisfmox = 10" in {
-        test("astabfulijqqxvzpxbbpisfmox", 10)
-    }
+  it should "awtmojhtslcnfdpffakysphq = 12" in {
+    test("awtmojhtslcnfdpffakysphq", 12)
+  }
+
+  it should "astabfulijqqxvzpxbbpisfmox = 10" in {
+    test("astabfulijqqxvzpxbbpisfmox", 10)
+  }
 
 }
