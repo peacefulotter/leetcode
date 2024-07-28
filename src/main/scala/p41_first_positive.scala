26253e80dbcbdb16dc19cde9425c55884ce44468
peacefulotter <zankro67@gmail.com>
2024-07-28 16:00:53 +0200

    Solved p41

--- src/main/scala/p41_first_positive.scala
+++ src/main/scala/p41_first_positive.scala
@@ -1,38 +1,28 @@
 package com.peacefulotter.leetcode
 
+import scala.util.boundary
+import scala.util.boundary.break
+
 object p41_first_positive {
-    // TODO: unsolved
-//    def firstMissingPositive(nums: Array[Int]): Int = {
-//        val p = nums.filter(n => n > 0 && n <= nums.length).toList
-//        println(nums.mkString("(", ", ", ")") + " " + p)
-//
-//        // filter <= 0
-//        @tailrec
-//        def rec(arr: List[Int], min: Int): Int = {
-//            println(arr + " " + min)
-//            arr match {
-//            case x :: xs if x > 0 => rec(xs, Math.min(min, x))
-//            case x :: xs if x <= 0 => rec(xs, min)
-//            case _ => min + 1
-//        }
-//        }
-//
-//        if (p.isEmpty) 1
-//        else if (p.size == nums.length) p.size + 1
-//        else rec(p, Integer.MAX_VALUE)
-//    }
-    def firstMissingPositive(nums: Array[Int]): Int = {
-        val arr = nums.filter(_ > 0).distinct
-        val arr2 = arr.filter(_ <= arr.length).toList
-
-        if ( arr2.isEmpty ) return 1
-        // not great
-        else if (arr2.size == arr.length) return arr.length + 1
-
-        val r = Range.inclusive(1, arr2.size + 1).map(_ => true).toBuffer
-        for (elt <- arr2; if elt <= arr2.size + 1)
-            r(elt - 1) = false
-        println(r)
-        r.zipWithIndex.find(_._1).get._2 + 1
+  def firstMissingPositive(nums: Array[Int]): Int = {
+    // Replace too low / high values with len+1
+    for (i <- Range(0, nums.length);
+         n = nums(i)
+         if n <= 0 || n > nums.length) {
+      nums.update(i, nums.length + 1)
     }
+
+    // Mark values using its negative
+    for (i <- Range(0, nums.length);
+         n = Math.abs(nums(i))
+         if n <= nums.length) {
+      nums.update(n - 1, -Math.abs(nums(n - 1)))
+    }
+
+    // First non negative is the one missing
+    boundary:
+      for (i <- Range(0, nums.length); n = nums(i); if n > 0)
+        break(i + 1)
+      nums.length + 1
+  }
 }
